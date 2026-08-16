63cb3b8502e9ca277ecb400be86f6850b186b6d7
Omar Atri <atriomar00@gmail.com>
2026-08-16 21:53:41 +0100

    Wrap pages in admin layout

--- src/app/(navbar)/admin/settings/page.tsx
+++ src/app/(navbar)/admin/settings/page.tsx
@@ -1,7 +1,11 @@
+import AdminLayout from "@/components/layouts/admin-layout";
+
 export default function AdminSettingsPage() {
 	return (
-		<div className="max-w-2xl pr-2 md:pr-10 pb-20 pl-2">
-			<h3 className="font-bold text-2xl">Settings</h3>
-		</div>
+		<AdminLayout title="Settings">
+			<div className="pr-2 md:pr-10 pb-20 pl-2 max-w-2xl">
+				<h3 className="font-bold text-2xl">Settings</h3>
+			</div>
+		</AdminLayout>
 	);
 }
--- src/app/(navbar)/admin/users/page.tsx
+++ src/app/(navbar)/admin/users/page.tsx
@@ -1,10 +1,10 @@
 import UsersTable from "@/components/admin/users-table";
+import AdminLayout from "@/components/layouts/admin-layout";
 
 export default function AdminUsersPage() {
 	return (
-		<div className="pr-2 md:pr-10 pb-20 pl-2">
-			<h3 className="mb-4 font-bold text-2xl">Users</h3>
+		<AdminLayout title="Users">
 			<UsersTable />
-		</div>
+		</AdminLayout>
 	);
 }
